9c2d586dbf0b0ed4db69ad3debd2f930c49d66ea
Astrid Avalin Sørensen <astrid.avalin.soerensen@consultant.lego.com>
2022-09-28 14:01:57 +0200

    - Better naming & added to Player object

---
+++ Assets/Scripts/Controls/CarWheelAnimations.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controls
+{
+    public class CarWheelAnimations : MonoBehaviour
+    {
+        [SerializeField] private List<Animator> wheels;
+
+        private void Awake()
+        {
+            SpinWheels();
+        }
+
+        public void SpinWheels()
+        {
+            foreach (Animator animator in wheels)
+            {
+                animator.enabled = true;
+            }
+        }
+
+        public void StopWheels()
+        {
+            foreach (Animator animator in wheels)
+            {
+                animator.enabled = false;
+            }
+        }
+    }
+}
